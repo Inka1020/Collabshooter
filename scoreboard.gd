68401f1f28a770bc2d523745f657d6070bde8533
isaac wj a <1277521@sd61learn.ca>
2024-11-01 13:06:14 -0700

    move files around and change enemy speed

--- scoreboard.gd
+++ scoreboard.gd
@@ -5,12 +5,12 @@ var lives = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$scoretext.text = "Your Score: " + str(score)
-	$lives.text = "Lives left: " + str(lives)
+	$scoretext.text = "Score: " + str(score)
+	$lives.text = "Lives: " + str(lives)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	lives = get_parent().get_node("player").lives
-	$scoretext.text = "Your Score: " + str(score)
-	$lives.text = "Lives left: " + str(lives)
+	$scoretext.text = "Score: " + str(score)
+	$lives.text = "Lives: " + str(lives)
